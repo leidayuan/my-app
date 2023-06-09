ee346701fd934c514709d5d18c3774aebc48915b
leidayuan <leida2010@163.com>
2023-06-10 00:08:04 +0800

    Initial commit

---
+++ src/test.js
@@ -0,0 +1,12 @@
+import './test.css';
+
+function test() {
+  return (
+    <div className="test">
+      <button>这是个按钮</button>
+      <p className='text'>这是一段说明</p>
+    </div>
+  );
+}
+
+export default test;
